220290d3dea26efa1fdcba3e905ed2508f150f87
JonasParsons <103849158+JonasParsons@users.noreply.github.com>
2022-07-01 09:10:21 -0600

    Delete content2.js

--- content2.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("imgLogo").src = "/img/logo.png";
-
-var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-link.type = 'image/x-icon';
-link.rel = 'shortcut icon';
-link.href = 'favicon.ico';
-document.getElementsByTagName('head')[0].appendChild(link);
