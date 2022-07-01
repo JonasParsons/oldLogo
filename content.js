8e783adf12be29cc38d05ebb0185b1999c096343
JonasParsons <103849158+JonasParsons@users.noreply.github.com>
2022-07-01 09:10:12 -0600

    Delete content.js

--- content.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("eServicesLogo").src = "/img/logo_v2.png";
-document.getElementById("eServicesLogoRight").src = "/img/logo_right.png";
-
-var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-link.type = 'image/x-icon';
-link.rel = 'shortcut icon';
-link.href = 'favicon.ico';
-document.getElementsByTagName('head')[0].appendChild(link);
